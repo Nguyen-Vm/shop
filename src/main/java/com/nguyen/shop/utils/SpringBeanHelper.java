dd7fbaf274faf83bd52f54f8d45bfb03bdbb5c66
nguyen <rwmingad@163.com>
2018-02-08 18:13:06 +0800

    SpringBeanHelper

---
+++ src/main/java/com/nguyen/shop/utils/SpringBeanHelper.java
@@ -0,0 +1,31 @@
+package com.nguyen.shop.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author RWM
+ * @date 2018/2/8
+ */
+public class SpringBeanHelper implements ApplicationContextAware {
+    private static final Logger logger = LoggerFactory.getLogger(SpringBeanHelper.class);
+    protected static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringBeanHelper.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz){
+        try {
+            return applicationContext.getBean(clazz);
+        }catch (NoSuchBeanDefinitionException e){
+            logger.warn("no qualifying bean of type: {}", clazz);
+            return null;
+        }
+    }
+}
