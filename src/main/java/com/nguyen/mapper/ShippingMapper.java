f95632822c5b0a60dbd6e8dfb0be3ae463fc23af
nguyen <rwmingad@163.com>
2018-02-08 16:06:46 +0800

    收获地址

--- src/main/java/com/nguyen/mapper/ShippingMapper.java
+++ src/main/java/com/nguyen/mapper/ShippingMapper.java
@@ -1,8 +1,12 @@
 package com.nguyen.mapper;
 
 import com.nguyen.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(Shipping record);
@@ -14,4 +18,13 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    int deleteByUserIdShippingId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    int updateByShipping(Shipping shipping);
+
+    Shipping selectByUserIdShippingId(@Param("userId") Integer userId, @Param("shippingId") Integer shippingId);
+
+    List<Shipping> selectByUserId(Integer userId);
+
 }
