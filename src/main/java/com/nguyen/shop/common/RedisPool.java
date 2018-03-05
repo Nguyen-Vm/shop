85adf25d75dc1658d10b90efa9cf10f45efbb784
nguyen <rwmingad@163.com>
2018-03-05 23:20:46 +0800

    Redis连接池初始化

---
+++ src/main/java/com/nguyen/shop/common/RedisPool.java
@@ -0,0 +1,62 @@
+package com.nguyen.shop.common;
+
+import com.nguyen.shop.utils.PropertiesUtil;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @author RWM
+ * @date 2018/3/5
+ */
+public class RedisPool {
+    private static JedisPool pool;
+    private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total", "20"));
+    private static Integer maxIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle", "10"));
+    private static Integer minIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle", "2"));
+
+
+    private static String redisIp = PropertiesUtil.getProperty("redis.ip");
+    private static Integer redisPort = Integer.parseInt(PropertiesUtil.getProperty("redis.port"));
+
+    private static Boolean testOnBorrow = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.borrow", "true"));
+    private static Boolean testOnReturn = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.return", "false"));
+
+    private static void initPool(){
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxTotal(maxTotal);
+        config.setMaxIdle(maxIdle);
+        config.setMinIdle(minIdle);
+
+        config.setTestOnBorrow(testOnBorrow);
+        config.setTestOnReturn(testOnReturn);
+
+        config.setBlockWhenExhausted(true);
+
+        pool = new JedisPool(config, redisIp, redisPort, 1000*2);
+    }
+
+    static {
+        initPool();
+    }
+
+    public static Jedis getResource(){
+        return pool.getResource();
+    }
+
+    public static void returnBrokenResource(Jedis jedis){
+        pool.returnBrokenResource(jedis);
+    }
+
+    public static void returnResource(Jedis jedis){
+        pool.returnResource(jedis);
+    }
+
+    /*public static void main(String[] args) {
+        Jedis jedis = pool.getResource();
+        jedis.set("nguyenkey", "nguyenvalue");
+        returnResource(jedis);
+
+        pool.destroy();//临时调用，销毁连接池中的的所有连接
+    }*/
+}
